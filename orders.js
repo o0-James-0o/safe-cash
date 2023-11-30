1ad2936959ccf3daf5eb0a9b4bde45c1b9dae81e
o0-James-0o <116586901+o0-James-0o@users.noreply.github.com>
2023-11-30 12:00:45 -0300

    Aplicação do JS - animações e tema escuro

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '8561',
+        paymentStatus: 'Due',
+        shipping: 'Peding'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '48347',
+        paymentStatus: 'Due',
+        shipping: 'Peding'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'PDelivered'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '81475',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
